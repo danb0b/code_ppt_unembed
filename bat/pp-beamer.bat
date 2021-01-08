62cf7b453edb039130d04a2b4197b88f238ad4c4
Dan Aukes <danaukes@gmail.com>
2021-01-07 20:43:00 -0700

    updated pp-pp-beamer

--- bat/pp-beamer.bat
+++ bat/pp-beamer.bat
@@ -9,10 +9,10 @@ set par2=%2
 IF ["%par1:~-2%"] == ["md"] ( set fname=%par1:~0,-3%)
 
 IF ["%par2%"] == ["pdf"] ( 
-pandoc -s -t beamer -V theme:Madrid -V colortheme:beaver --slide-level=2 %par1% -o %fname%.%par2%)
+pandoc %~dp0\header-includes.yaml -s -t beamer --pdf-engine=xelatex --slide-level=2 %par1% -o %fname%.%par2%)
 
 IF ["%par2%"] == ["tex"] ( 
-pandoc -s -t beamer -V theme:Madrid -V colortheme:beaver --slide-level=2 %par1% -o %fname%.%par2%)
+pandoc %~dp0\header-includes.yaml -s -t beamer --pdf-engine=xelatex --slide-level=2 %par1% -o %fname%.%par2%)
 
 IF ["%par2%"] == ["pptx"] ( 
 pandoc -s --slide-level=2 %par1% -o %fname%.%par2%)
